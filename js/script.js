00ac78acc2f25bda93a1ba8b1344bf0c3d879c3a
frolui <frolui29@gmail.com>
2020-12-25 15:57:59 +0300

    uprgrade code to lessons version

--- js/script.js
+++ js/script.js
@@ -1,29 +1,20 @@
 "use strict";
 
-
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '0');
 
 alert(numberOfFilms);
 
 const personalMovieDB = {
-    count : [],
-    movies : [],
-    actors : [],
+    count : {},
+    movies : {},
+    actors : {},
     genres : [],
-    privat : []
+    privat : false
 };
 
-personalMovieDB.count = numberOfFilms;
-personalMovieDB.movies = {};
-personalMovieDB.actors = {};
-personalMovieDB.genres = {};
-personalMovieDB.privat = false;
-
 const lastFilm = prompt('Last viewed movie',' ');
-alert(`last film is ${lastFilm}`);
 const assesment = prompt('Your assesment?','5');
-alert(`assesment is ${assesment}`);
-const movie = {};
 
 personalMovieDB.movies[lastFilm] = assesment;
+
 console.log(personalMovieDB);
