b77569c5e7a0018fad632f671ad4a1d05d6d773d
frolui <frolui29@gmail.com>
2020-12-26 21:00:41 +0300

    Completed task n3 from 2nd home task

--- js/script.js
+++ js/script.js
@@ -15,4 +15,13 @@ const assesment = prompt('Your assesment?','5');
 
 personalMovieDB.movies[lastFilm] = assesment;
 
+if (personalMovieDB.count < 10){
+    alert('Просмотрено довольно мало фильмов');
+}else if (personalMovieDB.count < 30){
+    alert('Вы классический зритель');
+}else if (personalMovieDB.count >= 30){
+    alert('Вы киноман');
+}else {alert('Произошла ошибка');}
+
+
 console.log(personalMovieDB);
